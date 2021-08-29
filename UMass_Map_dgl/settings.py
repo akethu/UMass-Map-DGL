4ed636c5733796da695171a4aba27a04b28f4e34
akethu <akethu@umass.edu>
2021-08-29 14:18:32 -0400

    change

--- UMass_Map_dgl/settings.py
+++ UMass_Map_dgl/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
