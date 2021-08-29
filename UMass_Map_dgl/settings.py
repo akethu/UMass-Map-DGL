69b3fe6e6304bed30ded8ed6b77fe5702d5a4f30
akethu <akethu@umass.edu>
2021-08-29 14:27:39 -0400

    changes

--- UMass_Map_dgl/settings.py
+++ UMass_Map_dgl/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
+# import os
 import django_heroku
 from pathlib import Path
 
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'UMass_Map_dgl.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
